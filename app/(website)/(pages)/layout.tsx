613eaceeb358324eaca53e66ed0ff4430d0b895f
MosesChisz <mosesc@cortexware.co.za>
2024-09-11 15:25:15 +0200

    eslint

--- app/(website)/(pages)/layout.tsx
+++ app/(website)/(pages)/layout.tsx
@@ -1,6 +1,7 @@
 import { MobileButton } from "@/components/customs/MobileButton";
 import Footer from "@/components/modules/footer";
 import Header from "@/components/modules/header";
+import * as React from "react";
 
 
 export default function RootLayout({
